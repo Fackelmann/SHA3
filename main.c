0e6ad46ba09c7e68db403aad6e333a998abc94ba
theninja029 <alessio.sarti99@gmail.com>
2015-04-24 22:36:00 +0200

    Improved printing
    Hi. I improved the output making the program usable to hash.

--- main.c
+++ main.c
@@ -11,8 +11,11 @@ int main(int argc, char *argv[]){
   int32_t *psize=&size;
   uint8_t *newmessage;
   newmessage=sponge((uint8_t *)argv[1],*psize);
+
   for(int32_t i=0;i<64;i++){
-    printf("%d %X\n",i,*(newmessage+i));
+    printf("%x",i,*(newmessage+i));
   }
+  printf("\n");
+
   return 0;
 }
